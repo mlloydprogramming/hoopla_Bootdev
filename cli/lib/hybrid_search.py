67acd99621da0791152ec24fbb05575dfafc4490
quitlloydering <mlloydprogramming@gmail.com>
2025-11-01 13:20:38 -0400

    implementing hybrid search functionality

---
+++ cli/lib/hybrid_search.py
@@ -0,0 +1,44 @@
+import os
+
+from .keyword_search import InvertedIndex
+from .search_utils import DEFAULT_SEARCH_LIMIT
+from .semantic_search import ChunkedSemanticSearch
+
+
+class HybridSearch:
+    def __init__(self, documents: list[dict]) -> None:
+        self.documents = documents
+        self.semantic_search = ChunkedSemanticSearch()
+        self.semantic_search.load_or_create_chunk_embeddings(documents)
+
+        self.idx = InvertedIndex()
+        if not os.path.exists(self.idx.index_path):
+            self.idx.build()
+            self.idx.save()
+
+    def _bm25_search(self, query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
+        self.idx.load()
+        return self.idx.bm25_search(query, limit)
+
+    def weighted_search(self, query: str, alpha: float, limit: int = 5) -> list[dict]:
+        raise NotImplementedError("Weighted hybrid search is not implemented yet.")
+
+    def rrf_search(self, query: str, k: int, limit: int = 10) -> list[dict]:
+        raise NotImplementedError("RRF hybrid search is not implemented yet.")
+
+
+def normalize_scores(scores: list[float]) -> list[float]:
+    if not scores:
+        return []
+
+    min_score = min(scores)
+    max_score = max(scores)
+
+    if max_score == min_score:
+        return [1.0] * len(scores)
+
+    normalized_scores = []
+    for s in scores:
+        normalized_scores.append((s - min_score) / (max_score - min_score))
+
+    return normalized_scores
